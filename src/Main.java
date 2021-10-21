bf2ebfb7d3bd0722c213c7538aec621df88488d2
Gatlin Farrington <gwfarrin@asu.edu>
2021-10-20 17:54:12 -0700

    FILE WRITER
    Implemented a file writer and created a text file to keep track of user info. We will use a FileReader to store all of the users in RAM when the program is started. This will be implemented by me in the next day or two.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Welcome to Mentcare!");
-
+		Signup s = new Signup();
+		s.writeInfo("Logan Farrington", 21, "29619 n 138th st", "602-810-4759", "ldfarrin@asu.edu");
 	}
 
 }
