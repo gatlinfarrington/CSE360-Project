4f5e46aa98fb6b4cf944cd27d25075b8b77fe131
Gatlin Farrington <gwfarrin@asu.edu>
2021-10-20 17:54:46 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Welcome to Mentcare!");
 		Signup s = new Signup();
-		s.writeInfo("Logan Farrington", 21, "29619 n 138th st", "602-810-4759", "ldfarrin@asu.edu");
+		Scanner scan = new Scanner(System.in);
+//		s.writeInfo("Logan Farrington", 21, "29619 n 138th st", "602-810-4759", "ldfarrin@asu.edu");
+		System.out.println("Please enter your 'name,age,address,phone,email.'");
+		String input = scan.next();
+		
 	}
 
 }
