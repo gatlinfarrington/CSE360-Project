2b4ab11a60ec598b8c1a62fe81f03a8317afd0dc
Vishok2001 <79304406+Vishok2001@users.noreply.github.com>
2021-11-15 02:38:32 -0700

    Update createAccount2.java

--- src/Screens/createAccount2.java
+++ src/Screens/createAccount2.java
@@ -1,3 +1,5 @@
+import java.awt.Insets;
+
 import javafx.application.Application;  
 import javafx.scene.Scene;  
 import javafx.scene.control.Button;  
@@ -6,7 +8,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;  
 import javafx.stage.Stage; 
 
-public class createAccount2 extends Application {  
+public class createAccount extends Application {  
   
       
 /*public static void main(String[] args) {  
@@ -22,20 +24,22 @@ public void start(Stage stage)
     Label Address = new Label("Address ");
     Label phoneNumber = new Label("Phone Number ");
     Label Email = new Label("Email ");
-    TextField tf1 = new TextField();  
-    TextField tf2 = new TextField();
-    TextField tf3 = new TextField();  
-    TextField tf4 = new TextField();
-    TextField tf5 = new TextField();
+    TextField textField1 = new TextField();  
+    TextField textField2 = new TextField();
+    TextField textField3 = new TextField();  
+    TextField textField4 = new TextField();
+    TextField textField5 = new TextField();
     
     Button create = new Button("Create");  
     //create.setOnAction(e->System.out.println("You entered: User_ID: "+tf1.getText()+""+"Password: "+tf2.getText()));  
-    GridPane root = new GridPane();  
-    root.addRow(0, name, tf1);
-    root.addRow(2, Age, tf2);
-    root.addRow(4, Address, tf3);
-    root.addRow(6, phoneNumber, tf4);
-    root.addRow(8, Email, tf5);
+    GridPane root = new GridPane();
+    root.setHgap(20);
+    root.setVgap(20);
+    root.addRow(0, name, textField1);
+    root.addRow(2, Age, textField2);
+    root.addRow(4, Address, textField3);
+    root.addRow(6, phoneNumber, textField4);
+    root.addRow(8, Email, textField5);
     root.addRow(10, create);  
     Scene scene = new Scene(root, 500, 500);  
     stage.setScene(scene);  
