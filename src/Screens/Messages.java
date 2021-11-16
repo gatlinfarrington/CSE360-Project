67d64a101f9c7569ab7c630416814914abdd6077
haydnvn <haydnavn@gmail.com>
2021-11-16 13:36:46 -0700

    Update Messages.java

--- src/Screens/Messages.java
+++ src/Screens/Messages.java
@@ -1,9 +1,12 @@
 package Screens;
+package Msg;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputControl
+import javafx.scene.control.TextField 
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
