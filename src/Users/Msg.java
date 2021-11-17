8bbc85d49810d3fc12bffa3b99d825734422c54e
haydnvn <haydnavn@gmail.com>
2021-11-16 22:33:49 -0700

    Add files via upload

--- src/Users/Msg.java
+++ src/Users/Msg.java
@@ -8,12 +8,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Msg {
-    Patient owner;
+    User owner;
     String filename;
 
-    public Msg(Patient p) {
+    public Msg(User p) {
         owner = p; // whose messages
-        this.filename = /*uniquePatientIdentifier*/ +"_Msgs.txt";
     }
 
     /*
@@ -22,21 +21,24 @@ public class Msg {
     called in the messages screen when a user writes a messages
     */
     public static void addMsg(User u, String c) {
-        //String name = u.getName;
-        msgToTxt(name, c);
+        String name = u.getName();
+        msgToTxt(name, c, u);
     }
 
     /*
     msgToTxt
     takes a message and adds it to the txt file where it is stored for later use
     */
-    public static void msgToTxt(String name, String content) {
+    public static void msgToTxt(String name, String content, User owner) {
+    	String filename = owner.Name + owner.Birthday +"_Msgs.txt";
         File msgFile = new File(filename);
         try {
             FileWriter fw = new FileWriter(msgFile, true);
             fw.write(name + " | " + content + "\n");
             fw.close();
             System.out.println("msg added: " + name);
+        }catch(Exception e) {
+        	
         }
     }
 
@@ -46,19 +48,26 @@ public class Msg {
     reads the message file and outputs an array list of all the messages so it can be
     used and shown on screen later
     */
-    public static ArrayList < String > readMsg() {
+    public static ArrayList < String > readMsg(User owner) {
+    	String filename = owner.Name + owner.Birthday +"_Msgs.txt";
+    	ArrayList < String > msgList = new ArrayList < String > ();
+    	try {
         BufferedReader br = new BufferedReader(new FileReader(filename));
         String line = br.readLine();
-        public static ArrayList < String > msgList = new ArrayList < String > ();
+        
 
         while (line != null) {
-            try {
                 msgList.add(line);
-            }
+
             line = br.readLine();
         }
         br.close();
         System.out.println("msgs init");
+    	}catch(Exception e) {
+    		
+    	}
+        
+        
 
         return msgList;
 
